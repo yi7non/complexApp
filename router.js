e9d40275bbb04903babb42278ab24e5a4c60725b
yinon elbaz <yi7non@gmail.com>
2022-06-19 00:22:33 +0300

    learned

--- router.js
+++ router.js
@@ -11,8 +11,8 @@ const followController = require('./controllers/followController')
 // router.post('/logout', userController.logout)
 
 // profile related routes
-router.get('/profile/:username', userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen)
-router.get('/profile/:username/followers', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen)
+// router.get('/profile/:username', userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen)
+// router.get('/profile/:username/followers', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen)
 router.get('/profile/:username/following', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowingScreen)
 
 // post related routes
