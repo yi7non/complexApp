d59abc9ee362b33a843404522f3cc2af61fa047a
yinon elbaz <yi7non@gmail.com>
2022-06-20 01:45:28 +0300

    i am here

--- router.js
+++ router.js
@@ -13,7 +13,7 @@ const followController = require('./controllers/followController')
 // profile related routes
 // router.get('/profile/:username', userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen)
 // router.get('/profile/:username/followers', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen)
-router.get('/profile/:username/following', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowingScreen)
+// router.get('/profile/:username/following', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowingScreen)
 
 // post related routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
