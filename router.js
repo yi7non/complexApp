a680147b354cc2af7da640632375d49154c231a0
yinon elbaz <yi7non@gmail.com>
2022-06-24 17:21:44 +0300

    1234

--- router.js
+++ router.js
@@ -17,8 +17,8 @@ const followController = require('./controllers/followController')
 
 // post related routes
 // router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
-router.post('/create-post', userController.mustBeLoggedIn, postController.create)
-router.get('/post/:id', postController.viewSingle)
+// router.post('/create-post', userController.mustBeLoggedIn, postController.create)
+// router.get('/post/:id', postController.viewSingle)
 router.get('/post/:id/edit', userController.mustBeLoggedIn, postController.viewEditScreen)
 router.post('/post/:id/edit', userController.mustBeLoggedIn, postController.edit)
 router.post('/post/:id/delete', userController.mustBeLoggedIn, postController.delete)
